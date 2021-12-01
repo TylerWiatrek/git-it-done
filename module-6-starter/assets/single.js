60e33c52442fb14d79536abf8befe40f1262f9c4
Tyler Wiatrek <wiatrektyler@yahoo.com>
2021-11-30 22:52:14 -0600

    added warning for more than 30 issues

--- module-6-starter/assets/single.js
+++ module-6-starter/assets/single.js
@@ -1,5 +1,6 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,9 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -15,6 +19,15 @@ var getRepoIssues = function(repo) {
     });
     
 };
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    limitWarningEl.appendChild(linkEl);
+}
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
